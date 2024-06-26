3a6b208d46009c14decc4f6589d910694b13653a
tavanthang <153381856+tavanthang@users.noreply.github.com>
2024-06-26 21:22:41 +0700

    Add files via upload

---
+++ Controllers/SinhViensController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using tvt_baikemtragiuaki.Models;
+
+namespace tvt_baikemtragiuaki.Controllers
+{
+    public class SinhViensController : Controller
+    {
+        private QLDEntities db = new QLDEntities();
+
+        // GET: SinhViens
+        public ActionResult tvtIndex()
+        {
+            var sinhVien = db.SinhVien.Include(s => s.Khoa);
+            return View(sinhVien.ToList());
+        }
+
+        // GET: SinhViens/Details/5
+        public ActionResult tvtDetails(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SinhVien sinhVien = db.SinhVien.Find(id);
+            if (sinhVien == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sinhVien);
+        }
+
+        // GET: SinhViens/Create
+        public ActionResult tvtCreate()
+        {
+            ViewBag.MAKHOA = new SelectList(db.Khoa, "MAKHOA", "TENKHOA");
+            return View();
+        }
+
+        // POST: SinhViens/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MASV,HOSV,TENSV,PHAI,NS,MAKHOA")] SinhVien sinhVien)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SinhVien.Add(sinhVien);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MAKHOA = new SelectList(db.Khoa, "MAKHOA", "TENKHOA", sinhVien.MAKHOA);
+            return View(sinhVien);
+        }
+
+        // GET: SinhViens/Edit/5
+        public ActionResult tvtEdit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SinhVien sinhVien = db.SinhVien.Find(id);
+            if (sinhVien == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MAKHOA = new SelectList(db.Khoa, "MAKHOA", "TENKHOA", sinhVien.MAKHOA);
+            return View(sinhVien);
+        }
+
+        // POST: SinhViens/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MASV,HOSV,TENSV,PHAI,NS,MAKHOA")] SinhVien sinhVien)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sinhVien).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MAKHOA = new SelectList(db.Khoa, "MAKHOA", "TENKHOA", sinhVien.MAKHOA);
+            return View(sinhVien);
+        }
+
+        // GET: SinhViens/Delete/5
+        public ActionResult tvtDelete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SinhVien sinhVien = db.SinhVien.Find(id);
+            if (sinhVien == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sinhVien);
+        }
+
+        // POST: SinhViens/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            SinhVien sinhVien = db.SinhVien.Find(id);
+            db.SinhVien.Remove(sinhVien);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
